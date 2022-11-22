4a9f9fd65e4cf763ac5dd30d4235255970a7b1c0
vzhev <v.zhevaga@gmail.com>
2022-11-22 01:39:23 +0100

    fixed Big img

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TEL-RAN_FE
 
-- ## [] hw_16_11 - ( [HTML](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/index.html), [ JS](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/script/script.js), [ CSS](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/style/style.css) ) [ \_"Preview gallery"\_](https://sl101.github.io/TEL-RAN_FE/hw_21_11/)
+- ## [] hw_16_11 - ( [HTML](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/index.html), [ JS](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/script/script.js), [ CSS](https://github.com/sl101/TEL-RAN_FE/blob/main/hw_21_11/style/style.css) ) [ \_"Preview gallery"\_](https://sl101.github.io/TEL-RAN_FE/blob/main/hw_21_11/)
 
 =========================================
 
--- hw_21_11/script/script.js
+++ hw_21_11/script/script.js
@@ -1,22 +1,29 @@
 const big_image = document.querySelector('.bottom__img');
 
 const top_content = document.querySelector('.content__top');
-
 const list = document.createElement('ul');
 list.classList.add('list');
 top_content.appendChild(list);
 
+const bottom_content = document.querySelector('.content__bottom');
+const bottom_img = document.createElement('img');
+bottom_img.classList.add('bottom__img');
+bottom_content.appendChild(bottom_img);
+
 const image_array = [
-	'media/ezhik.webp',
-	'media/kapatuch.webp',
-	'media/karuch.webp',
-	'media/krosh.webp',
-	'media/losiash.webp',
-	'media/nusha.webp',
-	'media/pin.webp',
-	'media/sovunia.webp',
-	'media/barash.webp',
+	'ezhik',
+	'kapatuch',
+	'karuch',
+	'krosh',
+	'losiash',
+	'nusha',
+	'pin',
+	'sovunia',
+	'barash',
 ];
+
+bottom_img.src = `media/${image_array[0]}.webp`;
+bottom_img.setAttribute.alt = `Picture ${image_array[0]}`;
 
 for (let i = 0; i < image_array.length; i++) {
 	const img_link = image_array[i];
@@ -24,11 +31,12 @@ for (let i = 0; i < image_array.length; i++) {
 	const item = document.createElement('li');
 	item.classList.add('item');
 	item.addEventListener('click', () => {
-		big_image.src = img_link;
+		bottom_img.src = `media/${img_link}.webp`;
 	});
 	const img = document.createElement('img');
 	img.classList.add('item__img');
-	img.src = img_link;
+	img.src = `media/${img_link}.webp`;
+	img.alt = `Picture ${img_link}`;
 	item.appendChild(img);
 	list.appendChild(item);
 }
