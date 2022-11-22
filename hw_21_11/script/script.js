8e3f6f8714f42de4efd9a6a36f8e64bd92c6e9c6
vzhev <v.zhevaga@gmail.com>
2022-11-22 22:23:24 +0100

    refactored js

--- hw_21_11/script/script.js
+++ hw_21_11/script/script.js
@@ -1,42 +1,41 @@
-const big_image = document.querySelector('.bottom__img');
+const image_array = [
+	'chiko',
+	'barry',
+	'carlin',
+	'krash',
+	'dokko',
+	'rosa',
+	'pin',
+	'olga',
+	'wally',
+];
 
 const top_content = document.querySelector('.content__top');
-const list = document.createElement('ul');
-list.classList.add('list');
-top_content.appendChild(list);
+const list_content = document.createElement('ul');
+list_content.classList.add('list');
+top_content.appendChild(list_content);
 
 const bottom_content = document.querySelector('.content__bottom');
 const bottom_img = document.createElement('img');
 bottom_img.classList.add('bottom__img');
 bottom_content.appendChild(bottom_img);
+setAttributes(bottom_img, image_array[0]);
 
-const image_array = [
-	'ezhik',
-	'kapatuch',
-	'karuch',
-	'krosh',
-	'losiash',
-	'nusha',
-	'pin',
-	'sovunia',
-	'barash',
-];
-
-bottom_img.src = `media/${image_array[0]}.webp`;
-bottom_img.setAttribute.alt = `Picture ${image_array[0]}`;
-
-for (let i = 0; i < image_array.length; i++) {
-	const img_link = image_array[i];
-	console.log(img_link);
+for (let img_link of image_array) {
 	const item = document.createElement('li');
 	item.classList.add('item');
-	item.addEventListener('click', () => {
-		bottom_img.src = `media/${img_link}.webp`;
-	});
+	item.onclick = () => setAttributes(bottom_img, img_link);
+
 	const img = document.createElement('img');
 	img.classList.add('item__img');
-	img.src = `media/${img_link}.webp`;
-	img.alt = `Picture ${img_link}`;
+	setAttributes(img, img_link);
+
 	item.appendChild(img);
-	list.appendChild(item);
+	list_content.appendChild(item);
+}
+
+function setAttributes(image_content, img_link) {
+	image_content.setAttribute('src', `media/${img_link}.webp`);
+	image_content.setAttribute('alt', `Picture ${img_link}`);
+	image_content.setAttribute('title', `${img_link}`);
 }
